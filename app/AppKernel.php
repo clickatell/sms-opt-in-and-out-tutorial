7faf8baf5b968c1ca1350776c1c4325a4d91fdd5
zachie.dubruyn <zachie@webunite.co.za>
2015-06-25 15:55:02 +0200

    Comment Added

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,7 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new Clickatell\SMSOptInAndOutBundle\ClickatellSMSOptInAndOutBundle(),
-            new \Clickatell\Symfony\ClickatellBundle(),
+            new Clickatell\Symfony\ClickatellBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
